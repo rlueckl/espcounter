6a51c12bc0d0d736b535a1bbcbc5263acd013320
Roland Lückl <roland.lueckl@1und1.de>
2023-12-21 10:49:53 +0100

    move micropython stuff to its own repo

--- micropython/main_dht22.py
+++
@@ -1,9 +0,0 @@
-import dht
-from time import sleep
-d = dht.DHT22(machine.Pin(2))
-while True:
-  d.measure()
-  temp = d.temperature()
-  humi = d.humidity()
-  print("temperature: {0}, humidity: {1}".format(temp, humi))
-  sleep(2)
